74a0c6c73f69fb84920c90f37de19e9d7596cfcd
Tianxin Tse <xietx1995@gmail.com>
2017-11-18 10:56:29 +0800

    add chapter: Variables

---
+++ 2017-11-18-Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaration */
+            short a;
+            int b;
+            double c;
+
+            /* actual initialization */
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+
+            /* Accepting Values from User */
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input is {0}", num);
+
+            Console.ReadKey();
+        }
+    }
+}
